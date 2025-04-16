4f78b2102b9f5d0653f95a82bc121a63b898c4a9
Jesús Germade <jesus@germade.dev>
2025-04-16 13:30:32 +0000

    some changes in vite structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const {
+  OUT_DIR = 'dist',
+} = process.env
+
 // https://vite.dev/config/
 export default defineConfig({
+  root: resolve(__dirname, 'src'),
+  publicDir: resolve(__dirname, 'static'),
+  build: {
+    outDir: resolve(__dirname, OUT_DIR),
+  },
   plugins: [vue()],
 })
